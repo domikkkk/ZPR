790f479150bc397cdb24ab476dfbd6529707d4b7
Dominik Sidorczuk <domik141202@gmail.com>
2024-04-13 00:49:37 +0200

    QtWidget works instead QMainWindow

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <mainwindow.hpp>
 #include <compare_files/cos.hpp>
 #include <QApplication>
+#include <QtWidgets>
 
 
 int main(int argc, char **argv) {
     QApplication a(argc, argv);
+    QWidget w;
+    w.show();
     return a.exec();
 }
