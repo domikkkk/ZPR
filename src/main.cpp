0bee1770256eef87ad22412b171b52e7ec94f54f
Dominik Sidorczuk <domik141202@gmail.com>
2024-04-12 20:22:56 +0200

    Add new folder for window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
+#include <mainwindow.hpp>
 #include <compare_files/cos.hpp>
+#include <QApplication>
 
 
-int main(void) {
-    return 0;
+int main(int argc, char **argv) {
+    QApplication a(argc, argv);
+    return a.exec();
 }
