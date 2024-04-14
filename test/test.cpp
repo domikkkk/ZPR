ce23ad85b29d3d06c72b87ede2855e5b4a5aedaf
Dominik Sidorczuk <domik141202@gmail.com>
2024-04-14 02:21:15 +0200

    Write test for kmp

--- test/test.cpp
+++ test/test.cpp
@@ -2,6 +2,21 @@
 #include <compare_files/kmp.hpp>
 
 
-int main(void) {
+TEST(KMPTest, BasicTest) {
+    std::string text = "ababcababcabcabc";
+    std::string pattern = "abc";
+    std::vector<int> expected_indexes = {2, 7, 10, 13};
+    std::vector<int> result_indexes = kmpSearch(text, pattern);
+
+    // Sprawdzenie czy wyniki są zgodne z oczekiwaniami
+    ASSERT_EQ(expected_indexes.size(), result_indexes.size());
+    for (size_t i = 0; i < expected_indexes.size(); ++i) {
+        ASSERT_EQ(expected_indexes[i], result_indexes[i]);
+    }
+}
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
