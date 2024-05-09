99f24f00eb1fc7be2342f18f7d541fd70e2cdd2d
Dominik Sidorczuk <domik141202@gmail.com>
2024-05-09 15:43:24 +0200

    Added function to generate text in mainwindow

--- include/mainwindow.hpp
+++ include/mainwindow.hpp
@@ -3,11 +3,20 @@
 
 #include <QMainWindow>
 
+
+struct Point {
+    int x;
+    int y;
+    Point(const int &x, const int &y);
+};
+
+
 class MainWindow : public QMainWindow {
     Q_OBJECT
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void gen_text(const QString &text, const int &size, const bool &if_bold = false);
 
 private:
 
--- src/app/mainwindow.cpp
+++ src/app/mainwindow.cpp
@@ -4,22 +4,14 @@
 #include <QWidget>
 
 
+Point::Point(const int &x, const int &y) {
+   this->x = x;
+   this->y = y;
+}
+
+
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
-   QLabel *welcomeLabel = new QLabel("Welcome!", this);
-   welcomeLabel->setAlignment(Qt::AlignHCenter);
-
-   QFont font = welcomeLabel->font();
-   font.setPointSize(24);
-   font.setBold(true);
-   welcomeLabel->setFont(font);
-   
-   QVBoxLayout *layout = new QVBoxLayout;
-   layout->addWidget(welcomeLabel);
-
-   QWidget *widget = new QWidget(this);
-   widget->setLayout(layout);
-
-   this->setCentralWidget(widget);
+   this->gen_text("Welcome!", 24, true);
    this->setWindowTitle("Files compare");
    this->resize(800, 600);
 }
@@ -27,4 +19,24 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
 
 MainWindow::~MainWindow() {
 
+}
+
+
+void MainWindow::gen_text(const QString &text, const int &size, const bool &if_bold) {
+   QLabel *Label = new QLabel(text, this);
+   Label->setAlignment(Qt::AlignHCenter);
+   // Label->setGeometry(200, 100, 600, 100);
+
+   QFont font = Label->font();
+   font.setPointSize(size);
+   font.setBold(if_bold);
+   Label->setFont(font);
+   
+   QVBoxLayout *layout = new QVBoxLayout;
+   layout->addWidget(Label);
+
+   QWidget *widget = new QWidget(this);
+   widget->setLayout(layout);
+
+   this->setCentralWidget(widget);
 }
