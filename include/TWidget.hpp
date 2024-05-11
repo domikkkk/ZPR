1f287386b643e9ebf13eae393ccc28942a62d367
Dominik <domik141202@gmail.com>
2024-05-11 17:52:55 +0200

    Added new class for displaying text

---
+++ include/TWidget.hpp
@@ -0,0 +1,14 @@
+#ifndef TWIDGET_HPP
+#define TWIDGET_HPP
+
+#include <QWidget>
+
+
+class TWidget : public QWidget {
+    Q_OBJECT
+public:
+    TWidget();
+};
+
+
+#endif
--- src/app/mainwindow.cpp
+++ src/app/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include <mainwindow.hpp>
 #include <QFileDialog>
 #include <algorithm>
+#include <TWidget.hpp>
 
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
@@ -11,14 +12,15 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
 
    this->layout = new QGridLayout;
 
-   this->add_button(new Button("Add file", 1, 0, this), &MainWindow::onButtonClicked);
+   this->add_button(new Button("Add file", 1, 1, this), &MainWindow::onButtonClicked);
 
-   this->add_button(new Button("Add file", 1, 2, this), &MainWindow::onButtonClicked);
+   this->add_button(new Button("Add file", 1, 3, this), &MainWindow::onButtonClicked);
 
    this->layout->setVerticalSpacing(20);
-   this->layout->addWidget(welcome, 0, 1);  // w 0 wierszu, w środkowej kolumnie 3 ustawiony napis
+   this->layout->addWidget(welcome, 0, 2);  // w 0 wierszu, w środkowej kolumnie ustawiony napis
 
    this->layout->setRowStretch(2, 1);  // Zrobienie wolnego wiersza bo dlaczego nie
+   this->layout->setColumnStretch(4, 1);
    for (int i = 0; i < this->layout->columnCount(); ++i) {
       this->layout->setColumnStretch(i, 1);
    }
@@ -51,15 +53,15 @@ QLabel *MainWindow::gen_text(const QString &text, const int &size, const bool &i
 
 
 void MainWindow::add_button(Button *button, void (MainWindow::*funtion)()) {
-   connect(button, &QPushButton::clicked, this, funtion);
+   this->connect(button, &QPushButton::clicked, this, funtion);
    this->layout->addWidget(button, button->row, button->column, 1, 1);
    this->buttons.push_back(button);
 }
 
 
 void MainWindow::remove_null_button() {
-   buttons.erase(std::remove_if(buttons.begin(), buttons.end(),
-    [](Button* button) { return button == nullptr; }), buttons.end());
+   this->buttons.erase(std::remove_if(this->buttons.begin(), this->buttons.end(),
+    [](Button* button) { return button == nullptr; }), this->buttons.end());
 }
 
 
@@ -80,7 +82,7 @@ void MainWindow::add_File(Button *button) {
    QLabel *fileLabel = this->gen_text(fileContent, 11);
    fileLabel->setMaximumWidth(this->width() / this->layout->columnCount());
    fileLabel->setAlignment(Qt::AlignLeft);
-   this->layout->addWidget(fileLabel, button->row, button->column, 2, 1);
+   this->layout->addWidget(fileLabel, button->row, button->column-1, 2, 2);
    this->layout->removeWidget(button);
    delete button;
 }
@@ -88,18 +90,18 @@ void MainWindow::add_File(Button *button) {
 
 void MainWindow::resizeEvent(QResizeEvent *event) {
    // QList<QWidget *> widgets = this->findChildren<QWidget *>();
-   QLayoutItem *item = this->layout->itemAtPosition(1, 0);
+   QLayoutItem *item = this->layout->itemAtPosition(1, 1);
    if (item) {
       QWidget *widget = item->widget();
       if (widget) {
-         widget->setMaximumWidth(this->width() / this->layout->columnCount());
+         widget->setMaximumWidth(2 * this->width() / this->layout->columnCount());
       }
    }
-   item = this->layout->itemAtPosition(1, 2);
+   item = this->layout->itemAtPosition(1, 3);
    if (item) {
       QWidget *widget = item->widget();
       if (widget) {
-         widget->setMaximumWidth(this->width() / this->layout->columnCount());
+         widget->setMaximumWidth(2 * this->width() / this->layout->columnCount());
       }
    }
 }
