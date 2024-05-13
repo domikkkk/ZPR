aeaeab52058f0a0d3b49fd222010bd8c750256d4
Dominik Sidorczuk <domik141202@gmail.com>
2024-05-13 16:03:02 +0200

    Added kmp extended test

--- test/test.cpp
+++ test/test.cpp
@@ -16,6 +16,19 @@ TEST(KMPTest, BasicTest) {
 }
 
 
+TEST(KMPTest, ExtendedTest) {
+    std::string text = "Bylo sobie bardzo zimno albo bardzo cieplo, to zalezalo od punktu widzenia bardzo";
+    std::string pattern = "bardzo";
+    std::vector<int> expected_indexes = {11, 29, 75};
+    std::vector<int> result_indexes = kmpSearch(text, pattern);
+
+    ASSERT_EQ(expected_indexes.size(), result_indexes.size());
+    for (size_t i = 0; i < expected_indexes.size(); ++i) {
+        ASSERT_EQ(expected_indexes[i], result_indexes[i]);
+    }
+}
+
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
